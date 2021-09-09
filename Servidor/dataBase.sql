c6dfb8141c8a2aa63e08ae40f2b34c01ef2da8b4
JhonatanD09 <joranada@gmail.com>
2021-09-08 21:05:21 -0500

    5 commit

---
+++ Servidor/dataBase.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE test3;
+  USE test3
+  CREATE TABLE users( id INT PRIMARY KEY, name VARCHAR(30) );
+  INSERT INTO users VALUES (1, 'Jose');
+  INSERT INTO users VALUES (2, 'Alex');
+  INSERT INTO users VALUES (3, 'Lola');
